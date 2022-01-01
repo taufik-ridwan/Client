8ea72e1941e8ba371377d09ec46bd0610c831002
Matejko06 <67696646+Matejko06@users.noreply.github.com>
2022-01-01 03:25:00 +0100

    BIG AAA

---
+++ src/main/java/mathax/client/utils/base91/Base91OutputStream.java
@@ -0,0 +1,48 @@
+package mathax.client.utils.base91;
+
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Base91OutputStream extends FilterOutputStream {
+    private int ebq = 0;
+    private int en = 0;
+
+    public Base91OutputStream(OutputStream out) {
+        super(out);
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        ebq |= (b & 255) << en;
+        en += 8;
+        if (en > 13) {
+            int ev = ebq & 8191;
+
+            if (ev > 88) {
+                ebq >>= 13;
+                en -= 13;
+            } else {
+                ev = ebq & 16383;
+                ebq >>= 14;
+                en -= 14;
+            }
+
+            out.write(Base91.ENCODING_TABLE[ev % Base91.BASE]);
+            out.write(Base91.ENCODING_TABLE[ev / Base91.BASE]);
+        }
+    }
+
+    @Override
+    public void write(byte[] data, int offset, int length) throws IOException {
+        for (int i = offset; i < length; ++i) write(data[i]);
+    }
+
+    @Override
+    public void flush() throws IOException {
+        if (en > 0) {
+            out.write(Base91.ENCODING_TABLE[ebq % Base91.BASE]);
+            if (en > 7 || ebq > 90) out.write(Base91.ENCODING_TABLE[ebq / Base91.BASE]);
+        }
+    }
+}
